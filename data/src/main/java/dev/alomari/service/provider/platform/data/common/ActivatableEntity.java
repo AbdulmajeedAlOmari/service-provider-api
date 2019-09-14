255126dfcce4a56fb0b44fc5bd3991fce32cf221
Abdulmajeed Alomari <abdulmajeedmmo@gmail.com>
2019-09-14 19:27:18 +0300

    fix: Add getters and setters to ActivatableEntity

--- data/src/main/java/dev/alomari/service/provider/platform/data/common/ActivatableEntity.java
+++ data/src/main/java/dev/alomari/service/provider/platform/data/common/ActivatableEntity.java
@@ -1,8 +1,13 @@
 package dev.alomari.service.provider.platform.data.common;
 
+import lombok.Setter;
+import lombok.Getter;
+
 import javax.persistence.Column;
 import javax.validation.constraints.NotNull;
 
+@Setter
+@Getter
 public class ActivatableEntity extends BaseEntity {
 
     @NotNull
