7729cdcd7d1679a2a1fd72c4e9b4c164a515f968
Abdulmajeed Alomari <abdulmajeedmmo@gmail.com>
2019-09-14 19:10:47 +0300

    fix: Add Repository annotation to Role and Privilege

--- data/src/main/java/dev/alomari/service/provider/platform/data/privilege/PrivilegeRepository.java
+++ data/src/main/java/dev/alomari/service/provider/platform/data/privilege/PrivilegeRepository.java
@@ -1,6 +1,8 @@
 package dev.alomari.service.provider.platform.data.privilege;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface PrivilegeRepository extends JpaRepository<Privilege, Integer> {
 }
