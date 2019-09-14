a07b0841b3b3367e6b2b5cad423eea80ae6ffcae
Abdulmajeed Alomari <abdulmajeedmmo@gmail.com>
2019-09-14 19:09:45 +0300

    feat: Add repositories for Roles and Privileges

---
+++ data/src/main/java/dev/alomari/service/provider/platform/data/privilege/PrivilegeRepository.java
@@ -0,0 +1,6 @@
+package dev.alomari.service.provider.platform.data.privilege;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PrivilegeRepository extends JpaRepository<Privilege, Integer> {
+}
---
+++ data/src/main/java/dev/alomari/service/provider/platform/data/role/RoleRepository.java
@@ -0,0 +1,6 @@
+package dev.alomari.service.provider.platform.data.role;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RoleRepository extends JpaRepository<Role, Integer> {
+}
