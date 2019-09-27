efdf55da91eda27c3af433d61acc8ac31e007d47
Abdulmajeed Alomari <abdulmajeedmmo@gmail.com>
2019-09-27 13:09:28 +0300

    feat: Make getters and setters for auditing entity

--- data/src/main/java/dev/alomari/service/provider/platform/data/common/AuditingEntity.java
+++ data/src/main/java/dev/alomari/service/provider/platform/data/common/AuditingEntity.java
@@ -1,23 +1,25 @@
 package dev.alomari.service.provider.platform.data.common;
 
+import lombok.Data;
 import org.springframework.data.annotation.CreatedBy;
 import org.springframework.lang.Nullable;
 
 import javax.persistence.Column;
 import java.util.Date;
 
+@Data
 public class AuditingEntity extends BaseEntity {
 
     @Nullable
     @CreatedBy
     @Column(name = "CREATED_BY")
-    String createdBy;
+    private String createdBy;
 
     @Nullable
     @Column(name = "ACTION_TAKEN_BY")
-    String actionTakenBy;
+    private String actionTakenBy;
 
     @Nullable
     @Column(name = "ACTION_TAKEN_AT")
-    Date actionTakenAt;
+    private Date actionTakenAt;
 }
