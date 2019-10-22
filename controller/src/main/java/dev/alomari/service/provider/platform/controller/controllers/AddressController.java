213c0de11f0d00d14d840e5bdedc2bf4bd04433b
Abdulmajeed Alomari <abdulmajeedmmo@gmail.com>
2019-10-22 23:15:50 +0300

    feat: Add endpoints with NOT_IMPLEMENTED tag

--- controller/src/main/java/dev/alomari/service/provider/platform/controller/controllers/AddressController.java
+++ controller/src/main/java/dev/alomari/service/provider/platform/controller/controllers/AddressController.java
@@ -6,14 +6,16 @@ import dev.alomari.service.provider.platform.data.address.Address;
 import dev.alomari.service.provider.platform.data.common.jsonviews.View;
 import dev.alomari.service.provider.platform.data.common.validation.groups.Validation;
 import dev.alomari.service.provider.platform.utility.constants.Routes;
+import dev.alomari.service.provider.platform.utility.exceptions.ServiceProviderError;
+import dev.alomari.service.provider.platform.utility.exceptions.ServiceProviderException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.awt.print.Pageable;
 
 @RestController
 @RequestMapping(Routes.ADDRESS_ROUTE_V1)
@@ -31,5 +33,37 @@ public class AddressController {
     @JsonView({ View.SimpleView.class })
     public ResponseEntity<Address> addAddress(@Validated(Validation.Input.class) @RequestBody Address address) {
         return ResponseEntity.ok(addressService.addAddress(address));
+    }
+
+    // TODO: Change privilege
+    @PreAuthorize("hasAuthority('ADDRESSES:ADD')")
+    @GetMapping
+    @JsonView({ View.SimpleView.class })
+    public ResponseEntity<Address> listAddresses(@PageableDefault Pageable pageable) {
+        throw new ServiceProviderException(ServiceProviderError.NOT_IMPLEMENTED);
+    }
+
+    // TODO: Change privilege
+    @PreAuthorize("hasAuthority('ADDRESSES:ADD')")
+    @GetMapping("/{id}")
+    @JsonView({ View.DetailedView.class })
+    public ResponseEntity<Address> viewAddress(@Validated(Validation.Input.class) @RequestBody Address address) {
+        throw new ServiceProviderException(ServiceProviderError.NOT_IMPLEMENTED);
+    }
+
+    // TODO: Change privilege
+    @PreAuthorize("hasAuthority('ADDRESSES:ADD')")
+    @PutMapping("/{id}")
+    @JsonView({ View.SimpleView.class })
+    public ResponseEntity<Address> updateAddress(@Validated(Validation.Input.class) @RequestBody Address address) {
+        throw new ServiceProviderException(ServiceProviderError.NOT_IMPLEMENTED);
+    }
+
+    // TODO: Change privilege
+    @PreAuthorize("hasAuthority('ADDRESSES:ADD')")
+    @DeleteMapping("/{id}")
+    @JsonView({ View.SimpleView.class })
+    public ResponseEntity<Address> deleteAddress(@Validated(Validation.Input.class) @RequestBody Address address) {
+        throw new ServiceProviderException(ServiceProviderError.NOT_IMPLEMENTED);
     }
 }
--- controller/src/main/java/dev/alomari/service/provider/platform/controller/controllers/ProposalController.java
+++ controller/src/main/java/dev/alomari/service/provider/platform/controller/controllers/ProposalController.java
@@ -6,6 +6,8 @@ import dev.alomari.service.provider.platform.data.common.jsonviews.List;
 import dev.alomari.service.provider.platform.data.common.jsonviews.View;
 import dev.alomari.service.provider.platform.data.order.proposal.Proposal;
 import dev.alomari.service.provider.platform.utility.constants.Routes;
+import dev.alomari.service.provider.platform.utility.exceptions.ServiceProviderError;
+import dev.alomari.service.provider.platform.utility.exceptions.ServiceProviderException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -27,23 +29,41 @@ public class ProposalController {
         this.proposalService = proposalService;
     }
 
-    @PreAuthorize("hasAuthority('ORDERS:VIEW')")
+    @PreAuthorize("hasAuthority('PROPOSALS:LIST')")
     @GetMapping
     @JsonView({ List.SimpleList.class })
     public ResponseEntity<Page<Proposal>> listAllProposals(@PageableDefault Pageable pageable) {
         return ResponseEntity.ok(proposalService.listAllProposals(pageable));
     }
 
-    @PreAuthorize("hasAuthority('ORDERS:VIEW')")
-    @GetMapping("/{proposalId}")
+    @PreAuthorize("hasAuthority('PROPOSALS:VIEW')")
+    @GetMapping("/{id}")
     @JsonView({ View.DetailedView.class })
-    public ResponseEntity<Proposal> viewProposal(@PathVariable Long proposalId) {
-        return ResponseEntity.ok(proposalService.viewProposal(proposalId));
+    public ResponseEntity<Proposal> viewProposal(@PathVariable Long id) {
+        return ResponseEntity.ok(proposalService.viewProposal(id));
     }
 
-    @PreAuthorize("hasAuthority('ORDERS:VIEW')")
+    @PreAuthorize("hasAuthority('PROPOSALS:ADD_ALL')")
     @PostMapping
     public ResponseEntity<Proposal> addProposal(@Valid @RequestBody Proposal proposal) {
         return ResponseEntity.ok(proposalService.addProposal(proposal));
+    }
+
+    @PreAuthorize("hasAuthority('PROPOSALS:REPLY')")
+    @PutMapping("/{id}/approve")
+    public ResponseEntity<Proposal> approveProposal(@PathVariable Long id) {
+        throw new ServiceProviderException(ServiceProviderError.NOT_IMPLEMENTED);
+    }
+
+    @PreAuthorize("hasAuthority('PROPOSALS:REPLY')")
+    @PutMapping("/{id}/reject")
+    public ResponseEntity<Proposal> rejectProposal(@PathVariable Long id) {
+        throw new ServiceProviderException(ServiceProviderError.NOT_IMPLEMENTED);
+    }
+
+    @PreAuthorize("hasAuthority('PROPOSALS:REPLY')")
+    @DeleteMapping("/{id}/cancel")
+    public ResponseEntity<Proposal> cancelProposal(@PathVariable Long id) {
+        throw new ServiceProviderException(ServiceProviderError.NOT_IMPLEMENTED);
     }
 }
